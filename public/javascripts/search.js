e5ce4a034452a29ac30749773fa938acd99ded9d
maddiedick <md47155@students.westport.k12.ct.us>
2015-08-13 14:23:58 -0400

    Added restrictions on the calendar

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -75,14 +75,40 @@ function drawMap() { //DRAWS THE GOOGLE MAP
 
 //description of Brown marker
   $.each(markerData,function(){
-    console.log(this)
+    //console.log(this)
+    //console.log(this.dates);
+    //alert(x);
     var latLong = new google.maps.LatLng(this.latitude, this.longitude);
     console.log(latLong)
 
-    //CREATES A VARIABLE COLLEGENAME AND SETS THE HEADER TO BE EQUAL TO THE NAME OF THE DATA ID
-    var collegeName = '<h4 class="center">' + this.name + '</h4>' + '<p class="flow-text">Book Your Tour</p>' + '<input id="calendar" type="text" class="center" style="float:left;width:25%;" placeholder="Pick Your Date"></input>';
-
+    //CREATES AN ARRAY OF AVAILABLE TIMES FOR TOURS FOR RESPECTIVE COLLEGES
+    //COLLEGES = [{'NAME':'HARVARD',...,'DATES':[{DATE: '8/15/15', TIMES:[10, 11, 1]}, {'DATE':...}]}]
     //CREATES THE CONTENT IN THE INFO WINDOW BASED ON THE COLLEGE INFO
+    //var availableDates = ["9-8-2015", "14-8-2015", "15-8-2015"];
+    if (this.name=="Brown University"){ //BROWN
+      var availableDates = ["24-8-2015", "27-8-2015", "15-8-2015"];
+      var availableTimes = ["10:00", "12:30", "3:30", "4:00"];
+    } else if (this.name=="Harvard University"){ //HARVARD
+      var availableDates = ["22-8-2015", "21-8-2015", "28-8-2015"];
+      var availableTimes = ["11:30", "1:00", "2:30"];
+    } else if (this.name=="University of Texas at Austin"){ //U TEXAS
+      var availableDates = ["31-8-2015", "15-8-2015", "20-8-2015"];
+      var availableTimes = ["9:00", "10:30", "1:30", "3:30"];
+    } else { //MICHIGAN
+      var availableDates = ["26-8-2015", "27-8-2015", "24-8-2015"];
+      var availableTimes = ["10:00", "12:30", "3:30", "4:00"];
+    }
+    //CREATES A VARIABLE COLLEGENAME AND SETS THE HEADER TO BE EQUAL TO THE NAME OF THE DATA ID
+    var collegeName = '<h4 class="center">' + this.name + '</h4>' + '<p class="flow-text">Book Your Tour</p>' +
+    '<input id="calendar" type="text" class="center" style="float:left;width:25%;" placeholder="Pick Your Date"></input>';
+
+
+    /*for(var i = 0; i < db.collegelist.length; i ++){
+      db.collegelist.find({"name" : 1});
+    }*/
+
+    //var availableDates = this.dates[0].date[0];
+
     var contentString = '<div id="content">'+
           '<div id="siteNotice">'+
           '</div>'+
@@ -95,7 +121,8 @@ function drawMap() { //DRAWS THE GOOGLE MAP
     var marker = new google.maps.Marker({
         position: latLong, //SETS THE LAT AND LONG
         map: map,
-        title: this.name //SETS THE TITLE EQUAL TO THE NAME
+        title: this.name, //SETS THE TITLE EQUAL TO THE NAME
+        //var availableDates = this.dates[0].date[0]
     });
 
     //var calendar = $(function() {$( "#calendar" ).datepicker();});
@@ -106,178 +133,32 @@ function drawMap() { //DRAWS THE GOOGLE MAP
 
         document.getElementById('bookVisit').innerHTML = collegeName; //GRABS THE DIV WITH THE ID OF BOOKVISIT AND SETS IT EQUAL TO THE VARIABLE COLLEGENAME
         //document.getElementById('calendar').innerHTML = calendar;
-        $(function() {
+        /*$(function() {
           $( "#calendar" ).datepicker(); //CREATES THE CALENDAR
-        });
+        });*/
+        function available(date){ //CREATES THE FUNCTION THAT TAKES THE PARAMETER DATE
+          dmy = date.getDate() + "-" + (date.getMonth()+1) + "-" + date.getFullYear(); //CREATES A VARIABLE THAT RETURNS DATE LIKE: DD-MM-YYYY
+          if ($.inArray(dmy, availableDates) != -1) { //I HAVE NO CLUE
+            return [true, "", "Available"]; //DATE IS AVAILABLE
+          } else {
+            return [false,"","unAvailable"]; //DATE IS UNAVAILABLE
+          }
+        }
+
+
+        /*function available(time){ //CREATES THE FUNCTION THAT TAKES THE PARAMETER DATE
+          '<input id="test1" type="radio"/><label for="test1">'time'</label>'
+          if ($.inArray(time, availableTimes) != -1) { //I HAVE NO CLUE
+            '<input type="radio" id="test1" /><label for="test1">' + time + '</label>'
+            return [true, "", "Available"]; //DATE IS AVAILABLE
+          } else {
+            return [false,"","unAvailable"]; //DATE IS UNAVAILABLE
+          }
+        } */
+        $('#calendar').datepicker({ beforeShowDay: available}); //SHOW AVAILABLE DATES
 
     });
   });
 }
 
 google.maps.event.addDomListener(window, 'load', initialize); //WHEN THE WINDOW LOADS, CALL ON THE INITIALIZE FUNCTION
-
-
-
-/*
-function makeMaker() {
-  //ADDING MARKERS
-  //DEFINES THE LAT AND LONG OF THE MARKERS POSITION
-  var myLatlng = new google.maps.LatLng(42.377003, -71.116660);
-  var myLatlngBrown = new google.maps.LatLng(41.826772,-71.402548); //COORDINATES FOR THE BROWN MARKER
-
-  //DEFINES THE CONTENT INSIDE THE POP UP WINDOW
-  var contentString = '<div id="content">'+
-      '<div id="siteNotice">'+
-      '</div>'+
-      '<h1 id="firstHeading" class="firstHeading">Harvard University</h1>'+
-      '<div id="bodyContent">'+
-      '<p><b>Harvard University</b>, is a private Ivy League research university in' +
-      '</br>Cambridge, Massachusetts, established in 1636. Its history, influence' +
-      '</br>and wealth have made it one of the most prestigious universities in the world</p>'+
-      '<p><a href="http://www.harvard.edu/">'+
-      'Harvard University</a> '+
-      '(last visited August 9, 2015).</p>'+
-      '</div>'+
-      '</div>';
-
-var contentStringBrown = '<div id="content">'+
-      '<div id="siteNotice">'+
-      '</div>'+
-      '<h1 id="firstHeading" class="firstHeading">Brown University</h1>'+
-      '<div id="bodyContent">'+
-      '<p><b>Brown University</b>, is a private Ivy League research university in' +
-      '</br>Providence, Rhode Island. Founded in 1764 as "The College in the' +
-      '</br>English Colony of Rhode Island and Providence Plantations"</p>'+
-      '<p> <a href="https://www.brown.edu/">'+
-      'Visit Brown University</a>.</p>'+
-      '</div>'+
-      '</div>';
-
-  //CREATES THE POP UP WINDOW WHEN THE MARKER IS PRESSED
-  var infowindow = new google.maps.InfoWindow({
-      content: contentString
-  });
-
-  var infowindowBrown = new google.maps.InfoWindow({
-    content: contentStringBrown
-  })
-
-  //CREATES THE MARKER
-  var marker = new google.maps.Marker({
-      position: myLatlng, //USES THE DEFINED LAT AND LONG FROM BEFORE
-      map: map, //MAP WAS DEFINED AT THE TOP OF THIS FILE
-      title: 'Harvard University' //TITLE
-  });
-
-  var markerBrown = new google.maps.Marker({
-    position: myLatlngBrown,
-    map: map,
-    title: 'Brown University'
-  })
-
-  //EVENT LISTENER FOR WHEN THE MARKER IS CLICKED
-  google.maps.event.addListener(marker, 'click', function() { //HARVARD
-    infowindowBrown.close();
-    infowindow.open(map,marker); //WHEN THE MARKER IS CLICKED OPEN THE WINDOW
-  });
-
-  google.maps.event.addListener(markerBrown, 'click', function() { //BROWN
-    infowindow.close();
-    infowindowBrown.open(map,markerBrown); //WHEN THE MARKER IS CLICKED OPEN THE WINDOW
-  });
-
-} */
-
-/*
-function initialize() {
-  //SEARCH BAR
-  var markers = [];
-  var map = new google.maps.Map(document.getElementById('map-canvas'), {
-  mapTypeId: google.maps.MapTypeId.ROADMAP,
-  center: {lat:39, lng:-98},
-  zoom:5
-  });
-
-  // Create the search box and link it to the UI element.
-  var input = /** @type {HTMLInputElement} *//*(
-    document.getElementById('pac-input'));
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-  var searchBox = new google.maps.places.SearchBox(
-  /** @type {HTMLInputElement} *//*(input));
-
-  // [START region_getplaces]
-  // Listen for the event fired when the user selects an item from the
-  // pick list. Retrieve the matching places for that item.
-  google.maps.event.addListener(searchBox, 'places_changed', function() {
-  var places = searchBox.getPlaces();
-
-  if (places.length == 0) {
-    return;
-  }
-  for (var i = 0, marker; marker = markers[i]; i++) {
-    marker.setMap(null);
-  }
-
-  // For each place, get the icon, place name, and location.
-  markers = [];
-  var bounds = new google.maps.LatLngBounds();
-  for (var i = 0, place; place = places[i]; i++) {
-    var image = {
-      url: place.icon,
-      size: new google.maps.Size(71, 71),
-      origin: new google.maps.Point(0, 0),
-      anchor: new google.maps.Point(17, 34),
-      scaledSize: new google.maps.Size(25, 25)
-    };
-
-    // Create a marker for each place from the search
-    var marker = new google.maps.Marker({
-      map: map,
-      icon: image,
-      title: place.name,
-      position: place.geometry.location
-    });
-
-    markers.push(marker);
-
-    bounds.extend(place.geometry.location);
-  }
-
-  map.fitBounds(bounds);
-  });
-
-  //markerData = [{collegename : 'Harvard University', latitude : 42.377003, longitude : -71.116660,
-  //collegedescription : 'Harvard University is a private Ivy League'}, {collegename : 'Brown University',
-  //latitude : 41.826772, longitude : -71.402548, collegedescription : 'Brown University is a great school'}];
-
-
-  //for(var i=0; i < markerData.length; i++){
-  $.each(markerData, function(){
-    //var data = markerData[i];
-    console.log(this)
-    var latLong = new google.maps.LatLng(this.latitude, this.longitude);
-
-    var contentString = '<div id="content">'+
-    '<div id="siteNotice">'+
-    '</div>'+
-    '<h1 id="firstHeading" class="firstHeading">' + this.collegename + '</h1>'+
-    '<div id="bodyContent">'+
-    '<p>' + this.collegedescription + '</p>' +
-    '</div>'+
-    '</div>';
-
-    var marker = new google.maps.Marker({
-      position: latLong, //USES THE DEFINED LAT AND LONG FROM BEFORE
-      map: map, //MAP WAS DEFINED AT THE TOP OF THIS FILE
-      title: this.collegename //TITLE
-    });
-
-    google.maps.event.addListener(marker, 'click', function() {
-      InfoWindow.close();
-      InfoWindow.setContent(contentString);
-      InfoWindow.open(map,marker);
-    });
-  });
-}
-*/
